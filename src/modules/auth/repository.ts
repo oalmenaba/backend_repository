2084548132910ca0547f6a842afd3de6d2a360f8
Ronny Endara <rojaence@gmail.com>
2024-12-18 21:19:36 -0500

    feat(FS/PATH): Agregado almacenamiento de usuarios en archivo json desde repository

--- src/modules/auth/repository.ts
+++ src/modules/auth/repository.ts
@@ -2,15 +2,39 @@ import { IUser } from "../../interfaces/Auth.interface";
 import fs from 'fs/promises'
 import path from 'path'
 
-const users: IUser[] = []
+const dataFilePath = path.join('src', 'data', 'users.json')
 
 export default class AuthRepository {
+
+  async readUsers(): Promise<IUser[] | Array<IUser>> {
+    try {
+      const data = await fs.readFile(dataFilePath, 'utf-8')
+      return JSON.parse(data)
+    } catch (error) {
+      return []
+    }
+  }
+
+  async writeUsers(users: IUser[]): Promise<void> {
+    try {
+      await fs.writeFile(dataFilePath, JSON.stringify(users, null, 2), { encoding: "utf-8" })
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log('Ha ocurrido un error al guardar los datos')
+        console.log(error.message)
+      }
+    }
+  }
+
   async createUser(user: IUser): Promise<IUser> {
+    const users = await this.readUsers()
     users.push(user)
+    await this.writeUsers(users)
     return user
   }
 
   async findByUserName(username: string) {
+    const users = await this.readUsers()
     return users.find(u => u.username.toLocaleLowerCase() === username.toLocaleLowerCase())
   }
 
